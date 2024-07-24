6bcf3458102a5119d13ab55f71f6e33a447786a2
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-24 08:11:33 +0530

    Initial commit

---
+++ Relational_DataBase/MySQL/CRUD.sql
@@ -0,0 +1,1 @@
+SELECT * FROM restaurants_finder.restaurants
---
+++ Relational_DataBase/MySQL/Inner_join.sql
@@ -0,0 +1,1 @@
+SELECT * FROM restaurants_finder.restaurants INNER JOIN restaurants_finder.addresses ON (restaurants.address_id = addresses.id)
